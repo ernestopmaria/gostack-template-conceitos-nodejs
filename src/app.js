29ad32450440f38e8013db71c05856ac14180700
Ernesto Maria <ernestomaria93@gmail.com>
2020-06-17 22:55:28 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,10 +22,10 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
  
    const {title, techs, url} = request.body;
-  const repository ={id:uuid(), title, techs, url, likes}
+  const repo ={id:uuid(), title, techs, url, likes}
 
- repositories.push(repository);
-return response.json(repository);
+ repositories.push(repo);
+return response.json(repo);
   // TODO
 });
 
@@ -33,12 +33,12 @@ app.put("/repositories/:id", (request, response) => {
   const {id} = request.params;
   const {title, techs, url} = request.body;
 
-  const repositoryIndex = repositories.findIndex(repository=>repository.id===id);
-  if (repositoryIndex < 0){
+  const repositorieIndex = repositories.findIndex(repo=>repo.id===id);
+  if (repositorieIndex < 0){
     return request.status(400).json({ error:'id not found'});
   }
-  const repo ={ id, title, techs, url};
- repositories[repositoryIndex] = repo;
+  const repo ={ id, title, techs, url,likes};
+ repositories[repositorieIndex] = repo;
 
  return response.json(repo);
 
@@ -49,11 +49,11 @@ app.put("/repositories/:id", (request, response) => {
 
 app.delete("/repositories/:id", (request, response) => {
   const {id} = request.params;
-  const repositoryIndex = repositories.findIndex(repo=>repo.id===id);
-  if (repositoryIndex < 0){
+  const repositorieIndex = repositories.findIndex(repo=>repo.id===id);
+  if (repositorieIndex < 0){
     return request.status(400).json({ error:'repositorie not found'})}
   
-    repositories.splice(repositoryIndex,1);
+    repositories.splice(repositorieIndex,1);
     return response.status(204).send();
 
 });
@@ -66,7 +66,7 @@ app.delete("/repositories/:id", (request, response) => {
     const repo = repositories.find( repository => repository.id === id );
   
     if (!repo) {
-      return response.status(400).send({error:'id dont exixt'});
+      return response.status(400).send();
     }
   
     repo.likes += 1;
