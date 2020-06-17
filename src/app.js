30ca72d4198c8be3a2798d2c8d486529dda3df0a
Ernesto Maria <ernestomaria93@gmail.com>
2020-06-17 22:22:57 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,28 +10,71 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
-const likes = [];
+const likes = 0;
 
 app.get("/repositories", (request, response) => {
+
+  return response.json(repositories);
   // TODO
-  const { title, url, techs} = request.body;
-  const repositories ={id:uuid(), tile:"desafio nodejs", url:"https://github.com/ernestopmaria/gostack-template-conceitos-nodejs"}
+
 });
 
 app.post("/repositories", (request, response) => {
+ 
+   const {title, techs, url} = request.body;
+  const repo ={id:uuid(), title, techs, url, likes}
+
+ repositories.push(repo);
+return response.json(repo);
   // TODO
 });
 
 app.put("/repositories/:id", (request, response) => {
+  const {id} = request.params;
+  const {title, techs, url} = request.body;
+
+  const repositorieIndex = repositories.findIndex(repo=>repo.id===id);
+  if (repositorieIndex < 0){
+    return request.status(400).json({ error:'id not found'});
+  }
+  const repo ={ id, title, techs, url,likes};
+ repositories[repositorieIndex] = repo;
+
+ return response.json(repo);
+
+
+
   // TODO
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositorieIndex = repositories.findIndex(repo=>repo.id===id);
+  if (repositorieIndex < 0){
+    return request.status(400).json({ error:'repositorie not found'})}
+  
+    repositories.splice(repositorieIndex,1);
+    return response.status(204).send();
+
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+
+
+ app.post("/repositories/:id/like", (request, response) => {
+    const { id } = request.params;
+  
+    const repo = repositories.find( repository => repository.id === id );
+  
+    if (!repo) {
+      return response.status(400).send();
+    }
+  
+    repo.likes += 1;
+  
+    return response.json(repo);
+  
+  });
+
+
 
 module.exports = app;
